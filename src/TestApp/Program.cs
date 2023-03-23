6591eaa3b5b032fa0e799e7a952165f52b98b93e
Hato1125 <hato.flavius@gmail.com>
2023-03-23 16:18:21 +0900

    マウスに関するクラスを作成

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -1,19 +1,29 @@
-﻿using SDLib;
+﻿using SDL2;
+using SDLib;
 using SDLib.Graphics;
+using SDLib.Input;
 using System.Drawing;
 
 namespace TestApp;
 
 internal class Program
 {
+    private static SDL.SDL_Event sdlEvent;
+
     [STAThread]
     static void Main()
     {
         var app = new Application("Test", new(1280, 720));
         
         app.OnInit += Init;
+        app.OnEvent += Event;
         app.OnRunning += Draw;
         app.Run();
+    }
+
+    private static void Event(SDL.SDL_Event e)
+    {
+        sdlEvent = e;
     }
 
     private static void Init(IntPtr renderer)
