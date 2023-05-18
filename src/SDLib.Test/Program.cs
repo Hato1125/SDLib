b16cbe83f692eafd24a57e15f4e0688308c71c6f
Hato1125 <hato.flavius@gmail.com>
2023-05-18 17:18:17 +0900

    Frameworkの削除

--- src/SDLib.Test/Program.cs
+++ src/SDLib.Test/Program.cs
@@ -7,13 +7,5 @@ internal class Program
     [STAThread]
     private static void Main()
     {
-        var app = new Game(
-            "Test",
-            SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN | SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE,
-            SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED,
-            new (1280, 720)
-        );
-
-        app.Run();
     }
 }
